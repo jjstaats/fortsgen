8a79dd967c9290167cd6dc7ebfbc37c2fd20b75a
Jonathan <maxcode@gamil.com>
2018-06-17 22:33:23 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace fortsgen
+{
+
+    public class Node
+    {
+        public int id;
+        public float x;
+        public float y;
+    }
+
+    class Program
+    {
+        public List<Node> Nodes;
+        
+        static void Main(string[] args)
+        {
+        }
+
+        public void Mutation(List<Node> blueprint)
+        {
+        }
+
+        public void Crossover(List<Node> blueprint1, List<Node> blueprint2)
+        {
+        } 
+    }
+}
